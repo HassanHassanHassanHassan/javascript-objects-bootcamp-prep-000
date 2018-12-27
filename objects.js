7b53ac95262fdc34667b77ab66e2d98536837004
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-27 00:05:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,10 +4,4 @@ playlist[artistName] = 'Beatles'
 playlist[songTitle] = 'Hey Jude'
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, 
-  playlist,
-  {[Beatles]:
-  'Hey Jude'
-    
-  }
-  )}
+  playlist[artistName] = 
