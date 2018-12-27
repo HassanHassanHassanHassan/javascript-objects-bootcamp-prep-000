fc0092e873d2cab189d14907a05b6ec55721d998
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-27 00:07:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ var playlist = new Object({Beatles: 'Hey Jude'});
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist }
+  
+  function removeFromPlaylist(playlist, artistName) {
+  playlist[artistName] = w
+  return playlist
+  }
