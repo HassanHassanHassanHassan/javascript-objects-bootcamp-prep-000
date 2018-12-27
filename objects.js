47f23a52897859988fbb857fff6dde4c13d2dac5
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-27 00:06:07 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,6 @@
 var playlist = new Object({Beatles: 'Hey Jude'});
 
-playlist[artistName] = 'Beatles'
-playlist[songTitle] = 'Hey Jude'
+
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
