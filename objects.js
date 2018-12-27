bba9c674ba304e1c9a5ee69fdbba03911d56faea
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-27 00:02:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
 var playlist = new Object({Beatles: 'Hey Jude'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, 
+  playlist,
+  {[Beatles]:
+  'Hey Jude'
+    
+  }
+  )}
